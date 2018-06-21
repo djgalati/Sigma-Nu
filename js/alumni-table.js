2949f4d79d7b4d2c7724904943f2b845978165dd
djgalati <38479396+djgalati@users.noreply.github.com>
2018-06-21 13:58:07 -0500

    Update alumni-table.js

--- js/alumni-table.js
+++ js/alumni-table.js
@@ -5,7 +5,7 @@ var visualization;
 
 function drawVisualization() {
     var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1il4lDznf6CeALokjqTe08wmppACX06vmjxT4G5oWYu0/edit?usp=sharing');
-    query.setQuery('SELECT A, B, C D label A "Pin #", B "Name", C "Pin #", D "Name"');
+    query.setQuery('SELECT A, B, I, J label A "Pin #", B "Name", I "Pin #", J "Name"');
     query.send(handleQueryResponse);
 }
 
