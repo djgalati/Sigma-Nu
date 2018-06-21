9fb7fd0e8147b86281a63fc82e69a4934bc2689c
djgalati <38479396+djgalati@users.noreply.github.com>
2018-06-20 21:49:08 -0500

    Create alumni-table.js

---
+++ js/alumni-table.js
@@ -0,0 +1,24 @@
+google.load('visualization', '1', {
+    packages: ['table']
+});
+var visualization;
+
+function drawVisualization() {
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1il4lDznf6CeALokjqTe08wmppACX06vmjxT4G5oWYu0/edit?usp=sharing');
+    query.setQuery('SELECT A, B label A "Pin #", B "Name"');
+    query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        allowHtml: true,
+        legend: 'bottom'
+    });
+}
+google.setOnLoadCallback(drawVisualization);
